fa75c47e56461d746c13de2f6e065bf0690dfaf3
Kevin <kevinohagan@hotmail.co.uk>
2019-05-10 11:03:53 +0100

    views and components setup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,23 @@
 <template lang="html">
-
+ <h1>Hello</h1>
 </template>
 
 <script>
 export default {
-}
+
+  name: 'app',
+  
+
+  components: {
+  chart,
+  portfolioValue,
+  List,
+  ListItem,
+  SearchBar
+  }
+
+},
+
 </script>
 
 <style lang="css" scoped>
---
+++ client/src/views/PortfolioView.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+   <div>
+    <List />
+   </div>
+</template>
+
+<script>
+import List from './components/List.vue';
+export default {
+  name: portfolioView,
+  data(){
+     return {
+       portfolio: []
+     }
+  },
+    components: {
+    List
+  },
+  mounted(){
+    this.fetchData();
+
+  },
+  methods: {
+    fetchData(){
+      fetch('http://localhost:3000/api/stocks')
+      .then(res => res.json())
+      .then(portfolio => this.portfolio = portfolio)
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>

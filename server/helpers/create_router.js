0184c011abce12777420664785b1f252cb49fc09
Danny Welsh <dannywelsh400@hotmail.com>
2019-05-10 11:02:22 +0100

    Creates fifth router for updating an entry

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -65,6 +65,26 @@ const createRouter = function(collection) {
     });
   });
 
+  // Update an Entry
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    collection
+    .findOneAndUpdate(
+      { _id: ObjectID(id) },
+      { $set: updatedData },
+      { returnOriginal: false }
+    )
+    .then((result) => {
+      res.json(result.value)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err})
+    });
+  });
+
   return router;
 }
 

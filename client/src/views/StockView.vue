733b3fcf416d8966445d17152a26276d0d57e3fd
Danny Welsh <dannywelsh400@hotmail.com>
2019-05-10 12:18:29 +0100

    Initial setup for StockView

---
+++ client/src/views/StockView.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <h1>{{stockInfo[companyName]}}</h1>
+</template>
+
+<script>
+export default {
+  name: "stockView",
+  // props: ["stock"],
+  data(){
+    return {
+      //selectedStock: this.stock,
+      stockInfo: {}
+    }
+  },
+  mounted(){
+    //if(!this.stock) this.$router.push('/stocks');
+
+    if(this.selectedStock) fetch('https://api.iextrading.com/1.0/stock/msft/batch?types=quote,news,chart&range=3m&last=10')
+    .then(response => response.json())
+    .then((details) => {
+      this.stockInfo = details;
+    })
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>

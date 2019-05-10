9c47f60426f4ec24e7adae90e2f996d3ae1a0e78
Danny Welsh <dannywelsh400@hotmail.com>
2019-05-10 10:41:19 +0100

    Creates second router for viewing a single result

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -20,6 +20,21 @@ const createRouter = function(collection) {
     });
   });
 
+  // View One
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+    .findOne({ _id: ObjectID(id) })
+    .then((doc) => {
+      res.json(doc)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
   return router;
 }
 

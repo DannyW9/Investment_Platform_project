31b5c2c4f188b47874240a356f2065528bc0c9fe
Danny Welsh <dannywelsh400@hotmail.com>
2019-05-11 12:28:36 +0100

    Adds graphjs as a dependency. Creates ChartService.js to test creating a chart - Potential to reuse the service to make multiple charts in different components

--- client/src/views/StockView.vue
+++ client/src/views/StockView.vue
@@ -1,20 +1,36 @@
 <template lang="html">
   <div class="">
-  <h1>{{stockInfo.companyName}} - ({{stockInfo.symbol}})</h1>
-  <h2>Sector: {{stockInfo.sector}}</h2>
-    </div>
+    <canvas id="planet-chart"></canvas>
+    <h1>{{stockInfo.companyName}} - ({{stockInfo.symbol}})</h1>
+    <h2>Sector: {{stockInfo.sector}}</h2>
+  </div>
 </template>
 
 <script>
+import Chart from 'chart.js';
+import chartTest from '@/services/ChartService.js'
 export default {
   name: "stockView",
   props: ["stock"],
   data(){
     return {
+      planetChartData: chartTest,
       selectedStock: this.stock,
       stockInfo: {}
     }
   },
+
+  methods: {
+    createChart(chartId, chartData) {
+      const ctx = chartId;
+      const myChart = new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options
+      });
+    }
+  },
+
   mounted(){
     if(!this.stock) this.$router.push('/stocks');
 
@@ -22,7 +38,9 @@ export default {
     .then(response => response.json())
     .then((details) => {
       this.stockInfo = details.quote;
-    })
+    });
+
+    this.createChart('planet-chart', this.planetChartData);
   }
 }
 </script>

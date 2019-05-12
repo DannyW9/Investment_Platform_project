c13f5d91432258446c2d7ca0b822a4c77d8e6412
Danny Welsh <dannywelsh400@hotmail.com>
2019-05-12 21:15:07 +0100

    Adds at a glance information and key statistics for each stock. Minor styling of the general page layout

--- client/src/views/StockView.vue
+++ client/src/views/StockView.vue
@@ -1,18 +1,62 @@
 <template lang="html">
   <div class="stock-info">
 
-    <h1>{{stockInfo.companyName}}</h1>
-    <h2>{{stockInfo.symbol}}</h2>
+    <h1>{{stockInfo.companyName}} ({{stockInfo.symbol}})</h1>
+    <h2>${{stockInfo.latestPrice}}</h2>
+    <h3>{{stockInfo.change}} ({{(stockInfo.changePercent)*100}}%)</h3>
     <p>Primary Exchange: {{stockInfo.primaryExchange}}</p>
     <canvas id="stock-price-chart"></canvas>
 
     <div id="stock-data">
-      <div id="left-div">
-        <h1>Hello Left</h1>
+
+      <div>
+        <h3 class="title">At a Glance</h3>
+        <div id="left-div">
+          <div>
+            <p class="heading">Open</p>
+            <p>${{stockInfo.open}}</p>
+            <hr>
+            <p class="heading">Previous Close</p>
+            <p>${{stockInfo.previousClose}}</p>
+            <hr>
+          </div>
+          <div class="inner-div">
+            <p class="heading">Volume</p>
+            <p>{{stockInfo.latestVolume}}</p>
+            <hr>
+            <p class="heading">Market Cap</p>
+            <p>{{stockInfo.marketCap}}</p>
+            <hr>
+          </div>
+        </div>
       </div>
-      <div id="right-div">
-        <h1>Hello Right</h1>
+
+      <div class="vertical"></div>
+
+      <div>
+        <h3>Key Statistics</h3>
+        <div id="right-div">
+          <div>
+            <p class="heading">P/E Ratio</p>
+            <p>{{stockInfo.peRatio}}</p>
+            <hr>
+            <p class="heading">Average Volume</p>
+            <p>{{stockInfo.avgTotalVolume}}</p>
+            <hr>
+          </div>
+          <div class="inner-div">
+            <p class="heading">52 Week Range</p>
+            <p>${{stockInfo.week52Low}} - ${{stockInfo.week52High}}</p>
+            <hr>
+            <p class="heading">YTD Change</p>
+            <p>{{stockInfo.ytdChange}}</p>
+            <hr>
+          </div>
+        </div>
       </div>
+        </div>
+      </div>
+
     </div>
 
   </div>
@@ -26,7 +70,6 @@ export default {
   props: ["stock"],
   data(){
     return {
-      // planetChartData: chartTest,
       selectedStock: this.stock,
       stockInfo: {},
       stockData: [],
@@ -46,6 +89,10 @@ export default {
       for(let data of this.stockData){
         this.labels.push(data.label);
       }
+    },
+
+    numberChange(x){
+      return x.toLocaleString('en');
     }
   },
 
@@ -68,6 +115,31 @@ export default {
 
 <style lang="css" scoped>
 
+.vertical {
+  border-left: 4px solid #FF6A00;
+  height: 200px;
+  padding: 10px;
+  margin-left: 50px;
+  margin-right: 50px;
+}
+
+.heading{
+  font-size: 12px;
+  text-transform: uppercase;
+}
+
+.inner-div {
+  margin-left: 30px;
+}
+
+#left-div, #right-div {
+  text-align: left;
+  line-height: 0.3;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
 .stock-info {
   text-align: center;
 }
@@ -81,7 +153,7 @@ canvas {
   display: flex;
   width: 100%;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: center;
 }
 
 </style>

31495ce53bb2b169751e87a25ebd277f5b0a9c85
JMMoir <jmmoir@outlook.com>
2019-05-13 22:04:23 +0100

    chart data pulling in from api

--- client/src/components/PortfolioChart.vue
+++ client/src/components/PortfolioChart.vue
@@ -2,12 +2,14 @@
   <div class="">
   <p>Total Value: ${{(this.totalValue).toFixed(2)}}</p>
   <div class="">
-  <canvas id="stock-price-chart"></canvas>
+  <canvas id="portfolio-chart"></canvas>
   </div>
   </div>
 </template>
 
 <script>
+import Chart from 'chart.js';
+import ChartService from '../services/ChartService.js';
 export default {
   name: 'Portfolio-chart',
   props:['portfolio'],
@@ -15,18 +17,23 @@ export default {
     return{
       quotes:[],
       values:[],
-      totalValue:0
+      totalValue:0,
+      chartData:[],
+      chartLabels:[],
+      charts:[]
     }
   },
 
   mounted(){
     const promises = this.portfolio.map((stock) => {
-    return fetch(`https://api.iextrading.com/1.0/stock/${stock.symbol}/batch?types=quote`)
+    return fetch(`https://api.iextrading.com/1.0/stock/${stock.symbol}/batch?types=quote,chart`)
     .then(res => res.json())
   })
     Promise.all(promises)
     .then(data => data.forEach((stock) => {
       this.quotes.push(stock['quote'])
+      this.charts.push(stock['chart'])
+      console.log(this.charts)
     }))
 
     .then(() => {
@@ -40,6 +47,9 @@ export default {
         return total + price
     })
   })
+
+
+  ChartService.createChart("portfolio-chart", this.chartData, this.chartLabels);
 }
 }
 </script>

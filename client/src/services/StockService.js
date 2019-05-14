e2aaf68d6b0a71485d32b0556fb0744156a289d8
Danny Welsh <dannywelsh400@hotmail.com>
2019-05-14 09:48:58 +0100

    fixes purchase button when shares are already owned. Both buttons now work as intended

--- client/src/services/StockService.js
+++ client/src/services/StockService.js
@@ -33,7 +33,7 @@ export default {
     return fetch(baseURL + id, {
       method: 'PUT',
       body: JSON.stringify(payload),
-      headers: { 'Content Type': 'application/json' }
+      headers: { 'Content-Type': 'application/json' }
     })
     .then(res => res.json())
   }
